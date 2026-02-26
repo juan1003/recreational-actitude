60a81246fdeecfa0d10c10238a3bd62ca4f45e58
Juan De Jesus <jdejesus93@gmail.com>
2026-02-25 23:41:09 -0600

    feat(map): Formatting using the correct language server

--- game/map.go
+++ game/map.go
@@ -3,23 +3,23 @@ package game
 type WeatherState string
 
 const (
-  Rain  WeatherState = "rain"
-  FairSkies WeatherState = "fair skies"
-  Thunderstorm WeatherState = "thunderstorm"
+	Rain         WeatherState = "rain"
+	FairSkies    WeatherState = "fair skies"
+	Thunderstorm WeatherState = "thunderstorm"
 )
 
 type Map struct {
-	Size int	
-	Coordinates [][] int
-	Name string
-	Weather WeatherState
+	Size        int
+	Coordinates [][]int
+	Name        string
+	Weather     WeatherState
 }
 
-func InitializeMap () *Map {
-	return &Map {
-		Size: 0,
+func InitializeMap() *Map {
+	return &Map{
+		Size:        0,
 		Coordinates: [][]int{},
-		Name: "",
-		Weather: FairSkies,	
+		Name:        "",
+		Weather:     FairSkies,
 	}
 }
