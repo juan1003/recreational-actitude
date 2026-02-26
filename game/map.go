629575a6380e21729af9b1f7841eb6703c8c9f25
Juan De Jesus <jdejesus93@gmail.com>
2026-02-25 19:55:52 -0600

    feat(map): Create a map module

---
+++ game/map.go
@@ -0,0 +1,25 @@
+package game
+
+type WeatherState string
+
+const (
+  Rain  WeatherState = "rain"
+  FairSkies WeatherState = "fair skies"
+  Thunderstorm WeatherState = "thunderstorm"
+)
+
+type Map struct {
+	Size int	
+	Coordinates [][] int
+	Name string
+	Weather WeatherState
+}
+
+func InitializeMap () *Map {
+	return &Map {
+		Size: 0,
+		Coordinates: [][]int{},
+		Name: "",
+		Weather: FairSkies,	
+	}
+}
